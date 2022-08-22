1b7140c038a2fcc9f28d3a975a9bfe60ca1d9385
Jacob Wagner <jvw5580@psu.edu>
2022-08-22 01:08:39 -0400

    Added event listener on update percent button to update percent values

---
+++ script.js
@@ -0,0 +1,36 @@
+
+// ---------------- Variables ---------------- //
+
+// grade vars
+const aGrade = document.querySelector('#aGrade');
+const bGrade = document.querySelector('#bGrade');
+const cGrade = document.querySelector('#cGrade');
+const dGrade = document.querySelector('#dGrade');
+const fGrade = document.querySelector('#fGrade');
+
+const gradeInputs = document.querySelectorAll('.gradeInput');
+
+const aPercent = document.querySelector('#aPercent');
+const bPercent = document.querySelector('#bPercent');
+const cPercent = document.querySelector('#cPercent');
+const dPercent = document.querySelector('#dPercent');
+const fPercent = document.querySelector('#fPercent');
+
+const gradeBtn = document.querySelector('#updateGradesBtn');
+
+// class vars
+
+// ---------------- Event Listeners ---------------- //
+
+gradeBtn.addEventListener('click', (e) => {
+    gradeInputs.forEach(function(grade, i) {
+        if (grade.value != "" && grade.value != null) {
+            if (i == 0) aPercent.innerHTML = grade.value + '%';
+            else if (i == 1) bPercent.innerHTML = grade.value + '%';
+            else if (i == 2) cPercent.innerHTML = grade.value + '%';
+            else if (i == 3) dPercent.innerHTML = grade.value + '%';
+            else  fPercent.innerHTML = grade.value + '%';
+        }
+        grade.value = "";
+    })
+});
